f866129b72bf8134b8c54e2e393d26e94d507484
shiwoo.park <sw.park@birdview.kr>
2022-09-04 23:51:27 +0900

    update

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## 기본 기능
 - 데이터 파싱
 - 데이터 검증: 타입, enum 등
+  - [pydantic](https://pydantic-docs.helpmanual.io/)
 - 자동 문서화 (API Spec)
   - [Swagger style](http://127.0.0.1:8000/docs) : `/docs`
   - [ReDoc style](http://127.0.0.1:8000/redoc) : `/redoc`
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import logging
+from datetime import datetime
 from typing import Union, Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from enums import ModelName
+
+# setup loggers
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+
+# get root logger
+logger = logging.getLogger(__name__)
+# the __name__ resolve to "main" since we are at the root of the project.
+# This will get the root logger since no logger in the configuration has this name.
 
 app = FastAPI()
 
@@ -71,5 +81,21 @@ class Item(BaseModel):
 
 
 @app.post("/items")
-async def create_item(item: Item) -> Item:
-    return item
+async def create_item(item: Item):
+    data = item.dict()
+    data["response_time"] = datetime.now()
+    return data
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    """
+    :param item_id: 아이템 ID, path param<br/>
+    :param item: 업데이트 할 아이템 정보<br/>
+    :param q: 쿼리 파라미터 예시. 디폴트값이 None 이기에 required 아닌것으로 인식<br/>
+    :return: 업데이트 후 아이템 정보
+    """
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
